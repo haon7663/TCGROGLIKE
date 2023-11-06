6aa26b2bf19a25a710ff6895b6276cabfdd94919
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-11-07 00:03:27 +0900

    Update ScriptableGrid.cs

--- TCG2/Assets/_Scripts/Grid/Scriptables/ScriptableGrid.cs
+++ TCG2/Assets/_Scripts/Grid/Scriptables/ScriptableGrid.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 using _Scripts.Tiles;
 using UnityEngine;
 
-namespace Tarodev_Pathfinding._Scripts.Grid.Scriptables {
-    public abstract class ScriptableGrid : ScriptableObject {
+namespace Tarodev_Pathfinding._Scripts.Grid.Scriptables 
+{
+    public abstract class ScriptableGrid : ScriptableObject 
+    {
         [SerializeField] protected NodeBase nodeBasePrefab;
         [SerializeField,Range(0,6)] private int _obstacleWeight = 3;
         public abstract Dictionary<Vector2, NodeBase> GenerateGrid();
