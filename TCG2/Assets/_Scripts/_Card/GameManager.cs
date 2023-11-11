a63e31456c69106c87fd701012a8d51830120de7
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-11-11 22:54:57 +0900

    ADD Card

---
+++ TCG2/Assets/_Scripts/_Card/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Inst { get; private set; }
+    void Awake() => Inst = this;
+
+    [SerializeField] NotificationPanel notificationPanel;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        InputCheatKey();
+#endif
+    }
+
+    void InputCheatKey()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            TurnManager.OnAddCard?.Invoke(true);
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+            TurnManager.OnAddCard?.Invoke(false);
+
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+            TurnManager.Inst.EndTurn();
+
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+            CardManager.Inst.TryPutCard(false);
+    }
+
+    void StartGame()
+    {
+        StartCoroutine(TurnManager.Inst.StartGameCo());
+    }
+
+    public void Nodification(string message)
+    {
+        notificationPanel.Show(message);
+    }
+}
